e91d14ee07c092e3348d3c0f1b614f85e1458372
sandwich <sandwich@live.cn>
2017-05-09 02:02:50 +0800

    day 5

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,12 +4,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.1)
- '(company-minimum-prefix-length 1))
+ '(company-minimum-prefix-length 1)
+ '(popwin:popup-window-position (quote right))
+ '(popwin:popup-window-width 90))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(iedit-occurrence ((t (:inherit region))))
+ '(region ((t (:inherit region :background "dark cyan")))))
 
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -19,6 +19,10 @@
 			    exec-path-from-shell
 			    popwin
 			    reveal-in-osx-finder
+			    web-mode
+			    js2-refactor
+			    expand-region
+			    iedit
 			    )  "Default packages")
 
 (setq package-selected-packages  sandwich/packages)
@@ -45,6 +49,7 @@
 
 ;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
 (smartparens-global-mode t)
+ (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
 
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
@@ -53,15 +58,49 @@
 (setq auto-mode-alist
       (append
        '(("\\.js\\'" . js2-mode))
+        '(("\\.html\\'" . web-mode))
        auto-mode-alist))
 
 (global-company-mode t)
+
+;; config for web mode
+(defun my-web-mode-indent-setup ()
+  (setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset 2)    ; web-mode, css in html file
+  (setq web-mode-code-indent-offset 2)   ; web-mode, js code in html file
+  )
+
+(defun my-toggle-web-indent ()
+  (interactive)
+  ;; web development
+  (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+      (progn
+	(setq js-indent-level (if (= js-indent-level 2) 4 2))
+	(setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))))
+
+  (if (eq major-mode 'web-mode)
+      (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+	     (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+	     (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
+  (if (eq major-mode 'css-mode)
+      (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+  (setq indent-tabs-mode nil))
+
+(global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
+
+;; config for js2-refactor
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
 
 (load-theme 'monokai t)
 
 (require 'popwin)
 (popwin-mode t)
 
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+(require 'iedit)
 
 (provide 'init-packages)
 
